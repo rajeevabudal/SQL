3c35a3789f631e64b46b0fdc2236371dae20401b
rajeeva <rajeevabp92@gmail.com>
2023-07-30 12:52:50 +0530

    Rising temperature

---
+++ Rising Temperature.sql
@@ -0,0 +1,76 @@
+-- 197. Rising Temperature
+
+-- Table: Weather
+
+-- +---------------+---------+
+
+-- | Column Name   | Type    |
+
+-- +---------------+---------+
+
+-- | id            | int     |
+
+-- | recordDate    | date    |
+
+-- | temperature   | int     |
+
+-- +---------------+---------+
+
+-- id is the primary key for this table.
+
+-- This table contains information about the temperature on a certain day.
+
+-- Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+-- Return the result table in any order.
+
+-- The query result format is in the following example.
+
+-- Example 1:
+
+-- Input:
+
+-- Weather table:
+
+-- +----+------------+-------------+
+
+-- | id | recordDate | temperature |
+
+-- +----+------------+-------------+
+
+-- | 1  | 2015-01-01 | 10          |
+
+-- | 2  | 2015-01-02 | 25          |
+
+-- | 3  | 2015-01-03 | 20          |
+
+-- | 4  | 2015-01-04 | 30          |
+
+-- +----+------------+-------------+
+
+-- Output:
+
+-- +----+
+
+-- | id |
+
+-- +----+
+
+-- | 2  |
+
+-- | 4  |
+
+-- +----+
+
+-- Explanation:
+
+-- In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+
+-- In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+# Write your MySQL query statement below
+select wt1.id
+from Weather wt1, Weather wt2
+where
+    wt1.temperature > wt2.temperature
+    and TO_DAYS(wt1.recordDate) - TO_DAYS(wt2.recordDate) = 1
